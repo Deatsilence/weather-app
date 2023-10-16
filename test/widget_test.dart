bc2a977222edec27ae28acadd44af93e2306361d
Mert Doğan <mertdogan@192.168.1.35>
2023-10-16 17:32:40 +0300

    network tests added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,44 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:f_weather/product/models/places_model.dart';
+import 'package:f_weather/product/models/weakly_forecast_model.dart';
+import 'package:f_weather/product/models/weather_forecast_model.dart';
+import 'package:f_weather/product/services/places_network_manager.dart';
+import 'package:f_weather/product/services/weather_network_manager.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:f_weather/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('dioGet should return a WeatherForecast', () async {
+    final networkManager = WeatherNetworkManager.instance;
+    final response = await networkManager.dioGet<WeatherForecast>(
+      end: '/weather',
+      queryParameters: {'name': 'Ankara'},
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // response, WeatherForecast tipinde bir nesne olmalıdır
+    expect(response, isNotNull);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  test('dioGet should return a WeaklyForecast', () async {
+    final networkManager = WeatherNetworkManager.instance;
+    final response = await networkManager.dioGet<WeaklyForecast>(
+      end: '/forecast',
+      queryParameters: {
+        "lat": 39.9199,
+        "lon": 32.8543,
+      },
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // response, WeatherForecast tipinde bir nesne olmalıdır
+    expect(response, isNotNull);
+  });
+
+  test('dioGet should return a Place', () async {
+    final networkManager = PredictionsNetworkManager.instance;
+    final response = await networkManager.dioGet<Places>('Ankara', Places());
+
+    // response, WeatherForecast tipinde bir nesne olmalıdır
+    expect(response, isNotNull);
   });
 }
